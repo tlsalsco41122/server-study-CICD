5857a9499a16f98321820d254733729f0ea05712
tlsalsco41122 <tlsalsco41122@gmail.com>
2026-06-17 09:23:02 +0900

    first commit

---
+++ src/test/java/com/example/Cicd_SpringBoot_01/CicdSpringBoot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Cicd_SpringBoot_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdSpringBoot01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
