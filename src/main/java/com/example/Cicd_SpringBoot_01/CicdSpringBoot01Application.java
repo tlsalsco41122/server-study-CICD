b82ff87934d9628d36e9487a4cb34749b1bbe675
tlsalsco41122 <tlsalsco41122@gmail.com>
2026-06-24 09:15:24 +0900

    gitignore

---
+++ src/main/java/com/example/Cicd_SpringBoot_01/CicdSpringBoot01Application.java
@@ -0,0 +1,13 @@
+package com.example.Cicd_SpringBoot_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdSpringBoot01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdSpringBoot01Application.class, args);
+	}
+
+}
